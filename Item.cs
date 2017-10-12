b07ff2762ecdb295ab046a6e46e080db54e9c527
MadhushreeGoniguntla <32753156+MadhushreeGoniguntla@users.noreply.github.com>
2017-10-12 19:24:24 -0400

    Create Item.cs

---
+++ Item.cs
@@ -0,0 +1,12 @@
+//Item Details
+    public class Item
+    {
+
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Author {get; set;}
+        public string Title {get; set;}
+        public double Price {get; set;}        
+        public int ItemTypeId {get; set;}                
+    }
+    
