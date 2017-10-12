c7e3d90362d4c894058315f07e7fa8777bd62854
MadhushreeGoniguntla <32753156+MadhushreeGoniguntla@users.noreply.github.com>
2017-10-12 19:23:45 -0400

    Create Member.cs

---
+++ Member.cs
@@ -0,0 +1,11 @@
+//Entity to represent borrower
+    public class Member
+    {
+
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Address {get; set;}
+        public string EmailId {get; set;}
+        public string Contact {get; set;}                       
+    }
