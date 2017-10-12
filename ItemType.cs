a1bbbfb1ae95f0f691da9ffd5f4e59c02a63d327
MadhushreeGoniguntla <32753156+MadhushreeGoniguntla@users.noreply.github.com>
2017-10-12 19:24:58 -0400

    Create ItemType.cs

---
+++ ItemType.cs
@@ -0,0 +1,9 @@
+//Item Type: General, reference, Children, CD/DVDs
+    public class ItemType
+    {
+        public int Id {get; set;}
+        public string Description {get; set;}
+        public int CheckOutDays {get; set;}
+        public int CheckOutLimit {get; set;}
+        public double FineAmount {get; set;}        
+    }
